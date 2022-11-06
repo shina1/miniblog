e575065f02543143c402f17daa34450f7d2d1bc2
shina1 <oswasiu@gmail.com>
2022-11-06 23:40:37 +0100

    completed databse fetch functionality

--- src/components/BlogMenu/BlogMenu.tsx
+++ src/components/BlogMenu/BlogMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { blogDataType, blogMenuData } from '../../utils/blogData';
 import { defaultImage } from '../../utils/defaultImage';
@@ -7,27 +7,45 @@ import { filterButtons } from '../../utils/filterButton';
 import './blog.css'
 
 // type settings for component props
+type cardDataType = {
+    blogAuthor : {name: string, id: string}
+    blogTitle: string,
+    category: string,
+    content : string,
+    date: string,
+    featured: string,
+    id: string,
+    imageUrl: string
+    }
+
 type blogMenuPropsType = {
     filtredData: string,
     setFilteredData: React.Dispatch<React.SetStateAction<string>>,
-    setCardData: React.Dispatch<React.SetStateAction<blogDataType[]>>,
-    cardData : blogDataType[] 
+    cardData : cardDataType[] 
 }
 
-const BlogMenuComp = ({filtredData, setFilteredData, setCardData, cardData}: blogMenuPropsType) => {
+// blogAuthor : {name: string, id: string}
+// blogTitle: string,
+// category: string,
+// content : string,
+// date: string,
+// featured: string,
+// id: string[],
+// imageUrl: string
+
+const BlogMenuComp = ({filtredData, setFilteredData, cardData}: blogMenuPropsType) => {
     
     let navigate = useNavigate()
 
     const handleSetData = (name: string) => {
         setFilteredData(name)
     }
-    const handleSetUrl = (id:number) => [
+    const handleSetUrl = (id:string) => [
         navigate(`/blog/${id}`)
     ]
-    
   return (
     <section className='max-w-[1640px] mx-auto px-4 py-12'>
-      <h2 className='text-orange-600 font-bold text-4xl text-center'>Top Rated</h2>
+      <h2 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Posts</h2>
       {/* filter row */}
       <div className='flex flex-col lg:flex-row items-center my-2'>
         {/* filter type */}
@@ -45,7 +63,7 @@ const BlogMenuComp = ({filtredData, setFilteredData, setCardData, cardData}: blo
       </div>
       {/* display food data */}
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
-        {
+        {/* {
             cardData.length === 0 && <div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
             <div className="animate-pulse flex space-x-4">
                     <div className="rounded-full bg-slate-700 h-10 w-10"></div>
@@ -61,17 +79,17 @@ const BlogMenuComp = ({filtredData, setFilteredData, setCardData, cardData}: blo
                         </div>
                     </div>
             </div>
-        }
+        } */}
         {
-             cardData.map((data)=> {
+             cardData && cardData.map((data)=> {
                return data.category.toLowerCase() === filtredData.toLowerCase() &&
-                <div key={data.id} className='relative border shadow-lg hover:scale-105 duration-300 rounded-lg'>
-                    <img src={data.image ? data.image : defaultImage} alt={data.name} className='w-full h-[200px] object-cover rounded-t-lg cursor-pointer' />
+                <div key={data.blogAuthor.id} className='relative border shadow-lg hover:scale-105 duration-300 rounded-lg'>
+                    <img src={data.imageUrl ? data.imageUrl : defaultImage} alt={data.blogAuthor.name} className='w-full h-[200px] object-cover rounded-t-lg cursor-pointer' />
                     {
                         data.featured ? <div className='flex items-center justify-center absolute right-0 top-2 text-[#fff] text[13px] bg-[#0F344E] w-[100px] p-1 rounded-l-xl'>Featured</div> : <div className='flex items-center justify-center absolute right-0 top-2 text-[#fff] text[13px] bg-orange-600 w-[100px] p-1 rounded-l-xl'>{data.category.charAt(0).toUpperCase() + data.category.slice(1)}</div> 
                     }
                     <div className='flex flex-col px-2 py-4 h-[300px]'>
-                        <p className='font-bold text-[24px] leading-2'>{data.name}</p>
+                        <p className='font-bold text-[24px] leading-2'>{data.blogAuthor.name}</p>
                         <p className='fifty-chars text-[15px] leading-2'>{data.content}</p>
                     </div>
                     <button className='flex items-center justify-center mt-1 mb-2 ml-2 mr-1 border-0 bg-[#FF7606]/80 text-[#fff] text-bold' onClick={() => handleSetUrl(data.id)}>Read More</button>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,48 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import BlogMenuComp from '../../components/BlogMenu/BlogMenu';
 import FooterComp from '../../components/FooterComp/Footer';
 import HeadlineComponent from '../../components/HeadlineComp/Headline';
 import HeroComponent from '../../components/HeroSection/Hero';
 import SubscriptionComp from '../../components/Newsletter/Subscribe';
-
+import {getDocs, collection} from "firebase/firestore"
 import { blogMenuData } from '../../utils/blogData'
+import { db } from '../../store/firebas-config';
+
+type cardDataType = {
+blogAuthor : {name: string, id: string}
+blogTitle: string,
+category: string,
+content : string,
+date: string,
+featured: string,
+id: string,
+imageUrl: string
+}
 
 const Home = () => {
-  const [cardData, setCardData] = useState(() => blogMenuData);
+  const [cardData, setCardData] = useState<cardDataType | any>();
   const [filtredData, setFilteredData] = useState('sports')
-  // blogMenuData
+  // fetch data from the database
+  const blogCollectionRef = collection(db, 'blogs');
+
+ useEffect(()=> {
+  const fetchBlogs = async () =>{
+      const result = await getDocs(blogCollectionRef)
+      if(result){
+        const refinedResult = result.docs.map((res) => ({...res.data(), id: res.id}))
+      setCardData(refinedResult);
+      }  
+  }
+  fetchBlogs()
+ },[])
+
+ 
+
   return (
     <div>
       <HeroComponent />
       <HeadlineComponent />
-      <BlogMenuComp filtredData={filtredData} setFilteredData={setFilteredData} setCardData={setCardData} cardData={cardData}/>
+      <BlogMenuComp filtredData={filtredData} setFilteredData={setFilteredData} cardData={cardData}/>
       <SubscriptionComp />
       <FooterComp />
     </div>
